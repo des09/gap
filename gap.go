15e6f0e458ca48b69cc95e2d74f7432299470b59
des09 <des.lownds@gmail.com>
2017-05-07 10:49:37 -0600

    adds sorting and aliases. improved tests

--- gap.go
+++ gap.go
@@ -16,19 +16,30 @@ import (
 	"text/tabwriter"
 
 	"github.com/reconquest/loreley"
+	"sort"
 )
 
 type holder struct {
-	pid   string
-	inode string
-	port  int64
-	ports string
-	cmd   []byte
-	lock  sync.Mutex
+	pid      string
+	inode    string
+	port     int64
+	ports    string
+	cmd      []byte
+	cmdColor int
+}
+type alias struct {
+	display []byte
+	regexp  *regexp.Regexp
+	color   int
 }
 
 var once sync.Once
 var proc = "/proc"
+var aliases = []alias{
+	{[]byte("webstorm"), regexp.MustCompile("java.*webstorm"), 51},
+	{[]byte("@1"), regexp.MustCompile(`^/usr/bin/([\w_\-\.]+)\s.*`), 48},
+	{[]byte("@1"), regexp.MustCompile(`^/usr/sbin/([\w_\-\.]+)\s.*`), 46},
+}
 
 //findPids returns a channel of FileInfo matching the pattern /proc/[pid]/
 // these are the directories containing process information in linux
@@ -37,7 +48,7 @@ func findPids() chan os.FileInfo {
 	go func() {
 		defer close(out)
 		if d, err := os.OpenFile(proc, os.O_RDONLY, 000); err != nil {
-			panic(err)  //bail if we can't read the /proc dir
+			panic(err) //bail if we can't read the /proc dir
 		} else {
 			fi, err := d.Readdir(-1)
 			handle(err)
@@ -105,6 +116,7 @@ func isInt(s string) bool {
 	}
 	return true
 }
+
 //mapPorts is a pipeline that fills in the port in the holder
 func mapPorts(portMap map[string]string, in <-chan holder) chan holder {
 	out := make(chan holder)
@@ -154,7 +166,7 @@ func grep(regexp *regexp.Regexp, in <-chan holder) chan holder {
 }
 
 //gather is a pipeline that reads all elements from the in pipe, groups them by pid,
-// and passes one holder per pid to out channel, with ports set
+// and passes one holder per pid to out channel, with ports set. Maintains the holder.port of the lowest in group, to preserve sort
 func gather(in <-chan holder) chan holder {
 	out := make(chan holder)
 	m := make(map[string]holder)
@@ -162,8 +174,12 @@ func gather(in <-chan holder) chan holder {
 	go func() {
 		for n := range in {
 			if v, t := m[n.pid]; t {
-				s := "," + strconv.FormatInt(n.port, 10)
-				v.ports = v.ports + s
+				if n.port > v.port {
+					v.ports = v.ports + "," + strconv.FormatInt(n.port, 10)
+				} else {
+					v.port = n.port
+					v.ports = strconv.FormatInt(n.port, 10) + "," + v.ports
+				}
 				m[n.pid] = v
 
 			} else {
@@ -180,10 +196,80 @@ func gather(in <-chan holder) chan holder {
 	return out
 }
 
+//sort is a pipeline that reads all elements from the in pipe, sorts them, then emits to out channel
+// the index is 0 = pid, 1 = port, 2 = cmd
+// the code assumes that ports and pids are smaller than 2^32 / 1000, so we take a small chance there... worst case sort order is off.
+func sortJ(in <-chan holder, index int) chan holder {
+	out := make(chan holder)
+	ms := make(map[string]holder)
+	mi := make(map[int]holder)
+
+	uniqi := func(i int) int {
+		ini := func(j int) bool {
+			_, ok := mi[j]
+			return ok
+		}
+		i = 1000 * i
+		for ini(i) {
+			i++
+		}
+		return i
+	}
+
+	uniqs := func(s string) string {
+		ini := func(j string) bool {
+			_, ok := ms[j]
+			return ok
+		}
+		for ini(s) {
+			s = s + "a"
+		}
+		return s
+	}
+
+	go func() {
+		for n := range in {
+			switch index {
+			case 0:
+				k, _ := strconv.Atoi(n.pid)
+				mi[uniqi(k)] = n
+			case 1:
+				i := int(n.port)
+				mi[uniqi(i)] = n
+			case 2:
+				k := string(n.cmd)
+				ms[uniqs(k)] = n
+			}
+		}
+
+		if len(ms) > 0 {
+			var keys []string
+			for k := range ms {
+				keys = append(keys, k)
+			}
+			sort.Strings(keys)
+			for _, k := range keys {
+				out <- ms[k]
+			}
+		} else {
+			var keys []int
+			for k := range mi {
+				keys = append(keys, k)
+			}
+			sort.Ints(keys)
+			for _, k := range keys {
+				out <- mi[k]
+			}
+		}
+		close(out)
+	}()
+	return out
+}
+
 func socketFromLink(p string) (string, bool) {
 	if s, e := os.Readlink(p); e == nil {
 		if strings.HasPrefix(s, "socket:[") {
-			s = s[8 : len(s) - 1]
+			s = s[8 : len(s)-1]
 			return s, true
 		}
 	}
@@ -195,17 +281,16 @@ func socketFromLink(p string) (string, bool) {
 // with \\n to buf per holder received on the in channel
 func emitBare(in <-chan holder, buf io.Writer) {
 	for p := range in {
-		if p.ports != "" {
-			fmt.Fprintf(buf, "%s\t%s\t\t%s\n", p.pid, p.ports, p.cmd)
-		} else {
-			fmt.Fprintf(buf, "%s\t%d\t\t%s\n", p.pid, p.port, p.cmd)
+		if p.ports == "" {
+			p.ports = strconv.FormatInt(p.port, 10)
 		}
+		fmt.Fprintf(buf, "%s\t%s\t\t%s\n", p.pid, p.ports, p.cmd)
 	}
 }
 
 //emitFormatted terminates the pipeline and writes a colorized and formatted
 // table to buf
-func emitFormatted(in <-chan holder, buf io.Writer){
+func emitFormatted(in <-chan holder, buf io.Writer) {
 
 	buffer := &bytes.Buffer{}
 
@@ -221,6 +306,9 @@ func emitFormatted(in <-chan holder, buf io.Writer){
 	for p := range in {
 		if p.ports == "" {
 			p.ports = strconv.FormatInt(p.port, 10)
+		}
+		if p.cmdColor > 0 {
+			p.cmd = []byte(fmt.Sprintf("<fg %d>%s", p.cmdColor, p.cmd))
 		}
 		writer.Write([]byte(strings.Join(
 			[]string{
@@ -245,15 +333,43 @@ func emitFormatted(in <-chan holder, buf io.Writer){
 		panic(err)
 	}
 
-	fmt.Fprint(buf,result)
+	fmt.Fprint(buf, result)
 
+}
 
+//mapAliases replaces cmd with the display from the first alias that matches cmd
+// and sets holder.cmdColor to the alias.cmdColor value. evaluates submatch replacement.
+func mapAliases(in chan holder) chan holder {
+	out := make(chan holder)
+	go func() {
+		for n := range in {
+			for _, r := range aliases {
+				if r.regexp.Match(n.cmd) {
+					result := r.regexp.FindStringSubmatch(string(n.cmd))
+					v := r.display
+					for i, el := range result {
+						t := []byte(fmt.Sprintf("@%d", i))
+						v = bytes.Replace(v, t, []byte(el), -1)
+					}
+
+					n.cmd = v
+					n.cmdColor = r.color
+				}
+			}
+			out <- n
+		}
+		close(out)
+	}()
+	return out
 }
 
 func main() {
 
 	commands := flag.BoolP("command", "c", false, "show commandlines")
 	bare := flag.BoolP("bare", "b", false, "clean output, ")
+	fsort := flag.IntP("sort", "s", -1, "sort, defaults to port if set with no value")
+	flag.Lookup("sort").NoOptDefVal = "1"
+	aliases := flag.BoolP("aliases", "a", false, "show aliases instead of full command")
 	grepStr := flag.StringP("grep", "g", "", "grep for command")
 	//kill := flag.Int("kill", false, "list commands")
 	tableOutput := flag.BoolP("table", "t", false, "output one line per port")
@@ -276,10 +392,17 @@ func main() {
 
 	if *grepStr != "" {
 		pipe = grep(regexp.MustCompile(*grepStr), mapCommands(pipe))
-	} else if *commands {
+	} else if *commands || *aliases {
 		pipe = mapCommands(pipe)
 	}
 
+	if *aliases {
+		pipe = mapAliases(pipe)
+	}
+
+	if *fsort >= 0 {
+		pipe = sortJ(pipe, *fsort)
+	}
 	out := bufio.NewWriter(os.Stdout)
 	if *bare {
 		emitBare(pipe, out)
@@ -299,7 +422,7 @@ func getTCPMap(path string, ret map[string]string) (err error) {
 	}
 
 	lines := strings.Split(string(content), "\n")
-	lines = lines[1 : len(lines) - 1]
+	lines = lines[1 : len(lines)-1]
 	words := make([]string, 20)
 	for _, line := range lines {
 		word := bytes.NewBuffer(make([]byte, 32))
@@ -326,7 +449,7 @@ func getTCPMap(path string, ret map[string]string) (err error) {
 				if _, t := ret[words[9]]; !t {
 					s := strings.Split(words[1], ":")
 					if len(s) > 1 {
-						p := s[len(s) - 1]
+						p := s[len(s)-1]
 						ret[words[9]] = p
 					}
 				}
--- gap_test.go
+++ gap_test.go
@@ -1,15 +1,19 @@
 package main
 
-import "testing"
 import (
 	"bytes"
 	"fmt"
 	"github.com/stretchr/testify/assert"
 	"regexp"
-	"time"
+	"testing"
 )
 
-func TestSomething(t *testing.T) {
+type ht struct {
+	h holder
+	t string
+}
+
+func TestGetTCPMap(t *testing.T) {
 	r := map[string]string{}
 
 	getTCPMap("spec/proc/net/tcp", r)
@@ -38,37 +42,52 @@ func TestShortPipe(t *testing.T) {
 }
 
 func TestEmitBare(t *testing.T) {
-	proc = "spec/proc"
 
-	buf := new(bytes.Buffer)
-	h := holder{pid:"1446",port:int64(6379)}
-	emitBare(from(h),buf)
+	tcs := []ht{
+		{h: holder{pid: "1446", port: int64(6379)}, t: "1446\t6379\t\t\n"},
+		{h: holder{pid: "1446", port: int64(6379), ports: "ports"}, t: "1446\tports\t\t\n"},
+		{h: holder{pid: "1446", port: int64(6379), cmd: []byte("cmd ")}, t: "1446\t6379\t\tcmd \n"},
+	}
 
-	s := buf.String()
-	assert.Equal(t, "1446\t6379\t\t\n",s)
+	for _, tc := range tcs {
+		buf := new(bytes.Buffer)
+		emitBare(from(tc.h), buf)
+
+		s := buf.String()
+		assert.Equal(t, tc.t, s)
+	}
+
 }
 
 func TestEmitFormatted(t *testing.T) {
-	proc = "spec/proc"
+	tcs := []ht{
+		{h: holder{pid: "1446", port: int64(6379)}, t: "pid   port\n1446  6379  \n"},
+		{h: holder{pid: "1446", port: int64(6379), ports: "ports"}, t: "pid   port\n1446  ports  \n"},
+		{h: holder{pid: "1446", port: int64(6379), cmd: []byte("cmd "), cmdColor: 22}, t: "pid   port\n1446  6379  cmd \n"},
+	}
 
-	buf := new(bytes.Buffer)
-	h := holder{pid:"1446",port:int64(6379)}
-	emitFormatted(from(h),buf)
+	for _, tc := range tcs {
+		buf := new(bytes.Buffer)
+		emitFormatted(from(tc.h), buf)
 
-	s := buf.String()
-	assert.Equal(t, "pid   port\n1446  6379  \n",s)
+		s := buf.String()
+		assert.Equal(t, tc.t, s)
+	}
 
 }
-func from(v holder)chan holder{
+
+func from(vi ...holder) chan holder {
 	in := make(chan holder)
-	go func(){
-		in <- v
+	go func() {
+		for _, v := range vi {
+			in <- v
+		}
 		close(in)
 	}()
 	return in
 }
 
-func TestCommands(t *testing.T) {
+func TestMapCommands(t *testing.T) {
 	proc = "spec/proc"
 	in := make(chan holder)
 	out := mapCommands(in)
@@ -82,9 +101,8 @@ func TestCommands(t *testing.T) {
 	assert.Equal(t, "", string(po.cmd))
 
 	close(in)
-	for range out {
-		assert.Fail(t, "out should be closed")
-	}
+	_, bad := <-out
+	assert.False(t, bad)
 }
 
 func TestGather(t *testing.T) {
@@ -92,7 +110,8 @@ func TestGather(t *testing.T) {
 	out := gather(in)
 	m := make(map[string]holder)
 
-	in <- holder{pid: "1446", port: int64(1)}
+	in <- holder{pid: "1446", port: int64(12)}
+	in <- holder{pid: "1446", port: int64(21)}
 	in <- holder{pid: "1446", port: int64(2)}
 	in <- holder{pid: "1447", port: int64(3)}
 
@@ -103,38 +122,70 @@ func TestGather(t *testing.T) {
 	}
 
 	assert.Len(t, m, 2)
-	assert.Equal(t, "1,2", m["1446"].ports)
+	assert.Equal(t, "2,12,21", m["1446"].ports)
+	assert.Equal(t, int64(2), m["1446"].port)
 	assert.Equal(t, "3", m["1447"].ports)
 }
 
-func TestGreps(t *testing.T) {
-	in := make(chan holder)
-	out := grep(regexp.MustCompile(".o."), in)
+func TestSortJ(t *testing.T) {
+	in1 := holder{pid: "1000", port: int64(100), cmd: []byte("a cmd")}
+	in2 := holder{pid: "2000", port: int64(200), cmd: []byte("b cmd")}
+	in3 := holder{pid: "3000", port: int64(300), cmd: []byte("c cmd")}
+	in11 := holder{pid: "1000", port: int64(100), cmd: []byte("a cmd")}
 
-	in <- holder{cmd: []byte("foo")}
-	in <- holder{}
-	in <- holder{cmd: []byte("bar")}
+	test := func(si int, f ...holder) {
+		in := make(chan holder)
+		out := sortJ(in, si)
+		for _, fi := range f {
+			in <- fi
+		}
+		close(in)
 
-	po, to := getOrTimeout(out, time.Millisecond*100)
-	assert.False(t, to)
-	assert.Equal(t, "foo", string(po.cmd))
-
-	po, to = getOrTimeout(out, time.Millisecond*10)
-	assert.True(t, to)
-
-	close(in)
-
-	for range out {
-		assert.Fail(t, "out should be closed")
+		assert.Equal(t, in1, <-out)
+		assert.Equal(t, in2, <-out)
+		assert.Equal(t, in3, <-out)
+		_, bad := <-out
+		assert.False(t, bad)
 	}
+
+	test(0, in1, in3, in2)
+	test(0, in2, in3, in1)
+	test(1, in1, in3, in2)
+	test(2, in1, in3, in2)
+
+	test2 := func(si int, f ...holder) {
+		in := make(chan holder)
+		out := sortJ(in, si)
+		for _, fi := range f {
+			in <- fi
+		}
+		close(in)
+		_, ok := <-out
+		assert.True(t, ok)
+		_, ok = <-out
+		assert.True(t, ok)
+		_, bad := <-out
+		assert.False(t, bad)
+	}
+	test2(0, in1, in11)
+	test2(1, in1, in11)
+	test2(2, in1, in11)
+
 }
 
-func getOrTimeout(c chan holder, d time.Duration) (ret holder, timedOut bool) {
-	select {
-	case res := <-c:
-		return res, false
-	case <-time.After(d):
-		fmt.Printf("timeout ")
-		return holder{}, true
+func TestGreps(t *testing.T) {
+	in := []holder{
+		{cmd: []byte("foo")},
+		{},
+		{cmd: []byte("bar")},
 	}
+
+	out := grep(regexp.MustCompile(".o."), from(in...))
+
+	po, ok := <-out
+	assert.True(t, ok)
+	assert.Equal(t, "foo", string(po.cmd))
+
+	po, extra := <-out
+	assert.False(t, extra)
 }
